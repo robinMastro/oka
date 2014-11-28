981adefa18aa916dcce7793b56f2ecdefdf601a6
Caroline Vien <caroline.vien8@gmail.com>
2014-11-28 12:40:42 +0100

    lien Mobile

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
         
         <script src="js/main.js"></script>
     </head>
-    <body onload="loader(); Anecdote(); detectmob()">
+    <body onload="loader(); Anecdote(); window.detectmob()">
       <div id="roue">
         <div class="windows8">
           <div class="wBall" id="wBall_1">
